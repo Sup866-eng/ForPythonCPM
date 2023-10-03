0306b58a2ab4d202bad209e448e07733fcb9c20c
Sup866-eng <66620561+Sup866-eng@users.noreply.github.com>
2023-10-03 18:10:29 +0300

    Update hw.py

--- new.py
+++ new.py
@@ -1,11 +1,15 @@
-a = int(input())
-b = int(input())
-for i in range(a, b+1):
-    count = 0
-    arr = []
-    for j in range(2, b+1):
-        if i % j == 0:
-            count+=1
-            arr.append(j)
-    if count == 3:
-        print("число: " + str(i) + "; " "делители:(" + str(1) + ", " + str(arr[0]) + ", " + str(arr[1]) + ", " + str(i) + ")")
+try:
+    a,b = map(int, input().split())
+    isinstance(a, int)
+    isinstance(b, int)
+    for i in range(a, b+1):
+        count = 0
+        arr = []
+        for j in range(2, b+1):
+            if i % j == 0:
+                count+=1
+                arr.append(j)
+        if count == 3:
+            print("число: " + str(i) + "; " "делители:(" + str(arr[-1]) + ", " + str(arr[1]) + ", " + str(arr[0]) + ", " + str(1) + ")")
+except:
+    print("Это не числа, попробуйте еще раз, пожалуйста")
