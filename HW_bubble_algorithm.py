e83d68c8839c37692499b47e964b73fc98d62f99
Sup866-eng <66620561+Sup866-eng@users.noreply.github.com>
2023-11-04 11:21:09 +0300

    Update HW_bubble_algorithm.py

--- HW_bubble_algorithm.py
+++ HW_bubble_algorithm.py
@@ -1,9 +1,6 @@
-arr = map(int, input())
+arr = list(map(int, input().split()))
 for i in range(1, len(arr)):
     for j in range(1, len(arr)):
-        a = arr[j]
-        b = arr[j+1]
-        if a > b:
-            a,b = b,a
-for i in arr:
-    print(arr[i])
+        if arr[j-1]>arr[j]:
+            arr[j-1], arr[j] = arr[j], arr[j-1]
+print(arr)
